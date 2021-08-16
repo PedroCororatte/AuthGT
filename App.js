61bb38f103ff563f9211326beb2a11979ad61001
Pedro Augusto Cororatte <pedroagt112@gmail.com>
2021-08-16 20:50:49 -0300

    add signup method

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, SafeAreaView, View, TextInput, ImageBackground, TouchableOpacity } from 'react-native';
 import styles from './styles';
-
+import firebase from './Firebase/Config'
 const bg = require('./resurces/girl.jpg')
 
 export default function App() {
+
+  const [Email, setEmail] = useState();
+  const [Password, setPassword] = useState();
+
+  const EmailSignUp = () => {
+    firebase.auth()
+      .createUserWithEmailAndPassword(Email, Password)
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+        console.error(error);
+      });
+  }
+
+  const test = () => console.warn(Email);
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style='light' />
@@ -14,23 +36,33 @@ export default function App() {
           <View style={styles.inputContainer}>
             <Text style={styles.title}>AuthGT</Text>
             <View>
-              <TextInput style={styles.input} placeholder='Email' placeholderTextColor='grey' />
-              <TextInput style={styles.input} placeholder='Password' placeholderTextColor='grey' />
+              <TextInput
+                style={styles.input}
+                placeholder='Email'
+                placeholderTextColor='grey'
+                onChangeText={text => setEmail(text)}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder='Password'
+                placeholderTextColor='grey'
+                onChangeText={text => setPassword(text)}
+              />
               <View style={styles.bottom}>
                 <View style={styles.buttons}>
                   <TouchableOpacity style={styles.button}>
                     <Text style={styles.buttonText}>Sign</Text>
                   </TouchableOpacity>
-                  <TouchableOpacity style={styles.button}>
+                  <TouchableOpacity style={styles.button} onPress={EmailSignUp}>
                     <Text style={styles.buttonText}>SignUp</Text>
                   </TouchableOpacity>
-                </View> 
+                </View>
                 <View style={styles.footer}>
-                    <Text style={styles.footerText}>Forgot password?</Text>
-                    <TouchableOpacity>
-                      <Text style={styles.textRecovery}>Recover</Text>
-                    </TouchableOpacity>
-                  </View>
+                  <Text style={styles.footerText}>Forgot password?</Text>
+                  <TouchableOpacity>
+                    <Text style={styles.textRecovery}>Recover</Text>
+                  </TouchableOpacity>
+                </View>
               </View>
             </View>
           </View>
